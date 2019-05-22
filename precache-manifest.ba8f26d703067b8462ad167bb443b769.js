6ff4fd50d49ba01e8da25f7db85bc1bffa4234f3
Ben Ely <ben.ely@h4bmanchester.com>
2019-05-22 11:15:05 +0100

    Updates

---
+++ precache-manifest.ba8f26d703067b8462ad167bb443b769.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3235f8ef10318355299365b5175e392",
+    "url": "/static/media/main-texture.c3235f8e.jpg"
+  },
+  {
+    "revision": "9e319ea4c3b722def175bf5ae52e9e24",
+    "url": "/static/media/festival-safe-logo.9e319ea4.png"
+  },
+  {
+    "revision": "2997496726d2cfed92c5b57304230cc1",
+    "url": "/static/media/fac50-use-protection.29974967.png"
+  },
+  {
+    "revision": "de32b2de09533265ec615653862df773",
+    "url": "/static/media/fac-50-bottle.de32b2de.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "046771fcbf5e66a49286",
+    "url": "/static/js/main.74202668.chunk.js"
+  },
+  {
+    "revision": "532bde333b2420df0997",
+    "url": "/static/js/2.856f6545.chunk.js"
+  },
+  {
+    "revision": "046771fcbf5e66a49286",
+    "url": "/static/css/main.6f3d2f13.chunk.css"
+  },
+  {
+    "revision": "0269a0397409072bb676f09ca662be37",
+    "url": "/index.html"
+  }
+];
